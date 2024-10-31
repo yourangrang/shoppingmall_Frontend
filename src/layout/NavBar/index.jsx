5fb9e99a74e6278aa1df20b99214e7a96170d984
yourangrang <163999418+yourangrang@users.noreply.github.com>
2024-10-31 17:44:36 +0900

    NavBar 구조생성

--- src/layout/NavBar/index.jsx
+++ src/layout/NavBar/index.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import NavItem from './Sections/NavItem';
 
 const NavBar = () => {
-  return (
-    <div>NavBar</div>
-  )
+    const [ menu, setMenu ] = useState(false);
+
+    const handleMenu = () => {
+        setMenu(!menu);
+    }
+
+    return (
+        <nav className='relative z-10 text-white bg-gray-900'>
+            <div className='w-full'>
+                <div className='flex items-center justify-between mx-5 sm:mx-10 lg:mx-20'>
+                    {/* logo */}
+                    <div className='flex items-center text-2xl h-14'>
+                        <Link to="/">Logo</Link>
+                    </div>
+                    {/* menu botton */}
+                    <div className='text-2xl sm:hidden'>
+                        <button onClick={handleMenu}>{menu ? "-" : "+" }</button>
+                    </div>
+                    {/* big screen nav-itmes */}
+                    <div className='hidden sm:block'>
+                        <NavItem />
+                    </div>
+                </div>
+                {/* small screen nav-itmes */}
+                <div className='block sm:hidden'>
+                    {menu && <NavItem />}
+                </div>
+
+            </div>
+        </nav>
+    )
 }
 
 export default NavBar
